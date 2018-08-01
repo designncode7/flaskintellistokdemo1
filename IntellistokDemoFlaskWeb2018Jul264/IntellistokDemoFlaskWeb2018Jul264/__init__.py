3db2a4e65de6bb0e9197d7413a1ef4722e8cb3df
Guss <designncode7@gmail.com>
2018-08-01 18:51:58 +0200

    Can send message via Server Sent Events SSE to Web App

--- IntellistokDemoFlaskWeb2018Jul264/IntellistokDemoFlaskWeb2018Jul264/__init__.py
+++ IntellistokDemoFlaskWeb2018Jul264/IntellistokDemoFlaskWeb2018Jul264/__init__.py
@@ -2,7 +2,28 @@
 The flask application package.
 """
 
-from flask import Flask
+from flask import Flask, Response
+import time
+from azure.servicebus import ServiceBusService, Message, Queue
+
 app = Flask(__name__)
+
+@app.route('/event_stream')
+def stream():
+    def event_stream():
+        while True:
+            bus_service = ServiceBusService(
+                service_namespace='svcbusqueintellidemo',
+                shared_access_key_name='RootManageSharedAccessKey',
+                shared_access_key_value='T5rIqHdGAM9/c1DtorXuyX6Rh3sxzHizri8UpNRqg+o=', 
+            )
+
+            iotmsg = bus_service.receive_queue_message('iothubqueuefri27-ns', peek_lock=False)
+
+            #time.sleep(3)
+            yield 'data: %s\n\n' % iotmsg.body
+
+    return Response(event_stream(), mimetype="text/event-stream")
+
 
 import IntellistokDemoFlaskWeb2018Jul264.views
--- IntellistokDemoFlaskWeb2018Jul264/IntellistokDemoFlaskWeb2018Jul264/views.py
+++ IntellistokDemoFlaskWeb2018Jul264/IntellistokDemoFlaskWeb2018Jul264/views.py
@@ -6,30 +6,28 @@ from flask import render_template
 from IntellistokDemoFlaskWeb2018Jul264 import app, errors
 from azure.servicebus import ServiceBusService, Message, Queue
 
-
 @app.route('/')
 @app.route('/home')
 def home():
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
 
-    bus_service = ServiceBusService(
-    service_namespace='svcbusqueintellidemo',
-    shared_access_key_name='RootManageSharedAccessKey',
-    shared_access_key_value='T5rIqHdGAM9/c1DtorXuyX6Rh3sxzHizri8UpNRqg+o='
-    )
+    #bus_service = ServiceBusService(
+    #service_namespace='svcbusqueintellidemo',
+    #shared_access_key_name='RootManageSharedAccessKey',
+    #shared_access_key_value='T5rIqHdGAM9/c1DtorXuyX6Rh3sxzHizri8UpNRqg+o=', 
+    #)
 
-    try:
-        iotmsg = bus_service.receive_queue_message('iothubqueuefri27-ns', peek_lock=False)
-    except Exception as e:
-          return render_template('500.html'), 500
+    #queue_options = Queue()
+    #queue_options.timeout = 1
+    #iotmsg = bus_service.receive_queue_message('iothubqueuefri27-ns', peek_lock=False)
+    
 
     """Renders the home page."""
     return render_template(
         'index.html',
-         title = "Intellistok Demo",
-         datetimeheader = "Data: ",
-         datetimecontent = iotmsg.body
+         title = "IS&P DT&IoT",
+         #datetimeheader = "Data: ",
+         #datetimecontent = "Testing",
+         year = datetime.now().year
     )
 
 
@@ -38,7 +36,7 @@ def about():
     """Renders the about page."""
     return render_template(
         'about.html',
-        title='About',
+        title='About This Web App',
         year=datetime.now().year,
-        message='Your application description page.'
+        message='Intellistok Demo Web Application created with Flask Micro Framework.'
     )
